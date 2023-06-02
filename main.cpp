769dbbe442aa24752474a7731b69de7af5066a93
Riddikuluz <francisco.robledov@utem.cl>
2023-06-02 15:50:57 -0400

    Change sku to nombre
    Change sku to nombre in the struct.
    FIX cumulative inflation.

--- main.cpp
+++ main.cpp
@@ -11,15 +11,14 @@ using namespace std;
 int main() {
     int cont = 0;
     string sku, monto, fecha, descuento, estado, wes, nombre, hora;
-
     struct sPro {
-        string vsku = "cod";
+        string vNombre = "nProd";
         vector<int> vMes = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         vector<int> vCant = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     };
     vector<sPro> arPro;
 
-    string dbSuper = "prueba1.csv";// supermercado - prueba
+    string dbSuper = "supermercado.csv";// supermercado - prueba1
     ifstream allData;
     allData.open(dbSuper);
     if (allData.fail()) {
@@ -33,7 +32,9 @@ int main() {
         //cout << "*" << records << "*" <<endl;
         if (records != ";" && records != "\n") {
             if (cont == 0)
-                sku = records;//else if (cont == 1)//nombre = records;
+                sku = records;
+            else if (cont == 1)
+                nombre = records;
             else if (cont == 2)
                 monto = records;
             else if (cont == 3)
@@ -49,24 +50,24 @@ int main() {
             //cout << "contador: " << cont << endl;
             cont++;
         }
-        if (cont == 6) {//una vez almacena los datos de la operacion
+        if (cont > 5) {//una vez almacena todos los datos de la operacion
             cont = 0;
             if ((estado == "AUTHORIZED" || estado == "FINALIZED") && sku != "0" && monto != "0" && descuento != "DESCUENTO"){
                 bool encontrado = false;
-                int auxstoi = stoi(fecha), valorF = stoi(monto) + stoi(descuento);// Consulta
-                /*
-                 cout << "\nsku: " << sku << endl;
-                 cout << "nombre: " << nombre << endl;
-                 cout << "monto: " << monto << endl;
-                 cout << "descuento: " << descuento << endl;
-                 cout << "fecha: " << fecha << endl;
-                 cout << "hora: " << hora << endl;
+                int auxstoi = stoi(fecha), valorF = stoi(monto);
+
+                /*cout << "\nsku: " << sku << endl;
+                cout << "nombre: " << nombre << endl;
+                cout << "monto: " << monto << endl;
+                cout << "descuento: " << descuento << endl;
+                cout << "fecha: " << fecha << endl;
+                cout << "hora: " << hora << endl;
                 cout << "estado: " << estado << endl;
-                cout << "Precione enter para continuar ...";
+                cout << "Presione enter para continuar ...";
                 cin.get();
-                */
+               */
                 for (int i = 0; i < arPro.size() && !encontrado; i++) {//busca si el producto esta en el struct
-                    if (arPro[i].vsku == sku) {
+                    if (arPro[i].vNombre == nombre) {
                         encontrado = true;
                         arPro[i].vCant[auxstoi - 1]++;
                         if (arPro[i].vMes[auxstoi - 1] == 0)
@@ -77,44 +78,55 @@ int main() {
                     }
                 }
                 if (!encontrado) {//si el producto no esta lo agrega
-                    vector<int> arxu = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-                    vector<int> canaxu = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-                    arxu[auxstoi - 1] = valorF;
-                    canaxu[auxstoi - 1]=1;
-                    sPro aux{sku, arxu, canaxu};
+                    vector<int> arrMonto = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+                    vector<int> arrCant = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+                    arrMonto[auxstoi - 1] = valorF;
+                    arrCant[auxstoi - 1]=1;
+                    sPro aux{nombre, arrMonto, arrCant};
                     arPro.push_back(aux);
                     //cout << " * sku NUEVA = " << arPro[arPro.size() - 1].vsku << " * mes = " << auxstoi << " * valor = "<< arPro[arPro.size() - 1].vMes[stoi(fecha) - 1] << endl;
                 }
             }
         }
     }
+
     allData.close();
 
+    //cout<< "N de productos = "<< arPro.size()<< endl;
     vector<double> sMes = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     for (int i = 0; i < arPro.size(); i++) {
         bool vacio = false;
+        //cout << arPro[i].vNombre<< endl;
         for (int j = 0; j < 12 && !vacio; j++) {
+            //cout << arPro[i].vMes[j]<<" - ";
             if (!arPro[i].vMes[j]) {
                 vacio = true;
             }
         }
-        if (!vacio)
+        if (!vacio){
+            //cout << arPro[i].vNombre<< endl;
             for (int k = 0; k < 12; k++) {
-                //cout << " * cantidad " << arPro[i].vCant[k] << endl;
+                //cout << " * cantidad " << arPro[i].vMes[k]<< endl;;
                 sMes[k] = sMes[k] + arPro[i].vMes[k] * arPro[i].vCant[k];
             }
-
+            //cout << "Presione enter para continuar ...";
+            //cin.get();
+        }
     }
+
+
     cout <<" * Variacion mensual"<< endl;
     double sumainflacion=0;
     vector<string> dMeses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
     for (int i = 0; i < 11; i++){
         cout << " * Suma total de los valores del mes de " << i+1 << " = " << sMes[i] << endl;
         cout << " * Indice del mes de " << dMeses[i+1] << " respecto del mes de " << dMeses[i] << " es igual a " << (sMes[i+1] / sMes[i]-1) * 100 << " %." << endl;
-        sumainflacion+=(sMes[i+1] / sMes[i]-1);
+        sumainflacion+=(sMes[i+1] / sMes[i]-1) * 100;
     }
-    //cout << " * Suma inflacion acumulada " << sumainflacion << " %." << endl;
-    cout << " * Indice del mes de " << dMeses[11] << " respecto del mes de " << dMeses[0] << " es igual a " << (sMes[11] / sMes[0]-1) * 100 << " %." << endl;
+
+    cout << " * Inflacion mensual acumulada es igual a " << sumainflacion << " %." << endl;
+
+    //cout << " * Indice del mes de " << dMeses[11] << " respecto del mes de " << dMeses[0] << " es igual a " << (sMes[11] / sMes[0]-1) * 100 << " %." << endl;
     //double inflacion = ((montoF - montoI) / montoI) * 100;
     //cout << " * N de productos " << arPro.size() << endl;
     //cout << montoF << " / " << montoI << endl;
